fba628153b8a253a5d29790e59ab724bd3577554
xuBi-n <99213248@qq.com>
2020-12-07 16:04:43 +0800

    comment Class

--- src/main/java/com/bdqn/main/Main.java
+++ src/main/java/com/bdqn/main/Main.java
@@ -1,23 +1,41 @@
 package com.bdqn.main;
 
+import com.bdqn.test.Demp;
+import com.bdqn.test.Person;
 import com.bdqn.util.Car;
 import com.bdqn.util.DB;
-import com.bdqn.util.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     //控制反转 IOC  面向切面 AOP（依赖注入）
     public static void main(String[] args) {
-        ApplicationContext ctx=new ClassPathXmlApplicationContext("sprint-autowire.xml");
-        DB db=ctx.getBean(DB.class);
-        System.out.println(db);
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("spring-config3.xml");
+        Person person=(Person) ctx.getBean("person");
+        Person person1=(Person) ctx.getBean("person2");
+        System.out.println(person);
+        System.out.println(person1);
 
-        Car car2= (Car) ctx.getBean("car2");
-        System.out.println(car2);
-//        Person person = ctx.getBean(Person.class);
 
+
+
+
+//        Person person=(Person)ctx.getBean("person");
+//        Person person1=(Person)ctx.getBean("person2");
+//        Person person2=(Person)ctx.getBean("person3");
 //        System.out.println(person);
-        ((ClassPathXmlApplicationContext)ctx).close();
+//        System.out.println(person1);
+//        System.out.println(person2);
+//        Demp demp= (Demp) ctx.getBean("demp");
+//        System.out.println(demp );
+//
+//
+////        Car car2= (Car) ctx.getBean("car2");
+////        System.out.println(car2);
+////        Person person = ctx.getBean(Person.class);
+//
+////        System.out.println(person);
+////        ((ClassPathXmlApplicationContext)ctx).close();
+//        ((ClassPathXmlApplicationContext)ctx).close();
     }
 }
